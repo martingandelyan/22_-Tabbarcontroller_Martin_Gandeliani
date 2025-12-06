0b65466982f22c70676501006a8f69915d10a866
Martin Gandeliani <martingandelyan@gmail.com>
2025-12-06 23:14:01 +0400

    Initial Commit

---
+++ 22_ Tabbarcontroller_Martin_Gandeliani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  22_ Tabbarcontroller_Martin_Gandeliani
+//
+//  Created by Martin on 06.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
